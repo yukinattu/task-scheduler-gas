b041c918b37fd7ec21691dfb8ac653c40251cd88
yukinattu <phononeue@gmail.com>
2025-06-07 23:01:21 +0900

    Update task-scheduler-gas.js

--- task-scheduler-gas.js
+++ task-scheduler-gas.js
@@ -4,7 +4,7 @@ const fetch = (...args) => import("node-fetch").then(({ default: fetch }) => fet
 
 puppeteer.use(StealthPlugin());
 
-const WEBHOOK_URL = "https://script.google.com/macros/s/AKfycbyUYFIXRDjTnidH5ZGeI-39BqlwsMuxELhV_XnBlqGTN_J1Vz--gl5Wr3mNBlaM79U1/exec";
+const WEBHOOK_URL = "https://script.google.com/macros/s/AKfycbzf2b7Fs6wQQ13-zHdTvGjfBQwGMFXD8i0td-TzgRWGOBzdPUkxSWKV0-mB8OhTPzwu/exec";
 const EXISTING_URLS_API = WEBHOOK_URL;
 const TIKTOK_USERS = ["nogizaka46_official", "kurumin0726"];
 
