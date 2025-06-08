347f6bfa315fe52d126271e30b0c1dd406c3f5e9
yukinattu <phononeue@gmail.com>
2025-06-08 15:19:18 +0900

    Update insta-feed-scraper.js

--- insta-feed-scraper.js
+++ insta-feed-scraper.js
@@ -6,7 +6,7 @@ puppeteer.use(StealthPlugin());
 
 const WEBHOOK_URL = "https://script.google.com/macros/s/AKfycbxtWswB_s3RZDCcA45dHT2zfE6k8GjaskiT9CpaqEGEvmPtHsJrgrS7cQx5gw1qvd8/exec";
 const EXISTING_URLS_API = WEBHOOK_URL;
-const INSTAGRAM_USERS = ["nogizaka46_official", "a_n_o2mass", "yasu.ryu9chakra"];
+const INSTAGRAM_USERS = ["nogizaka46_official", "a_n_o2mass", "yasu.ryu9chakra", "takato_fs"];
 
 function extractPostId(url) {
   const match = url?.match(/\/p\/([\w-]+)/);
