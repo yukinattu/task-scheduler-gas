3c0cf858cef0844589c43069e3afd14cf9ef4099
yukinattu <phononeue@gmail.com>
2025-06-09 21:17:34 +0900

    Update insta-reels-scraper.js

--- insta-reels-scraper.js
+++ insta-reels-scraper.js
@@ -7,7 +7,7 @@ puppeteer.use(StealthPlugin());
 
 const WEBHOOK_URL = "https://script.google.com/macros/s/AKfycbxtWswB_s3RZDCcA45dHT2zfE6k8GjaskiT9CpaqEGEvmPtHsJrgrS7cQx5gw1qvd8/exec";
 const EXISTING_URLS_API = WEBHOOK_URL;
-const INSTAGRAM_USER = "nogizaka46_official";
+const INSTAGRAM_USER = "jypetwice_japan";
 const REELS_URL = `https://www.instagram.com/${INSTAGRAM_USER}/reels/`;
 const FEED_URL = `https://www.instagram.com/${INSTAGRAM_USER}/`;
 
